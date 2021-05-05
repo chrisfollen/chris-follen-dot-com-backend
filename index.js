25b2180cc319e3b544923b8cd29a3dd615dcc7c9
Chris Follen <christopher.follen@gmail.com>
2021-05-05 10:48:59 -0600

    added post route

--- index.js
+++ index.js
@@ -2,6 +2,86 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const knex = require('knex')
+const config = require('./knexfile')['development']
+const database = knex(config)
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/journal', (request, response) => {
+    database('articles')
+        .then(articles => response.json(articles))
+})
+
+app.get('/journal/:slug', (request, response) => {
+    database('articles')
+        .where({slug: request.params.slug})
+        .then(articles => response.json(articles[0]))
+})
+
+app.post('/journal', (request, response) => {
+    database('articles')
+        .insert({
+            title: request.body.title,
+            date: request.body.date,
+            slug: request.body.slug,
+            featured: request.body.featured,
+            body1: request.body.body1,
+            body2: request.body.body2,
+            body3: request.body.body3,
+            body4: request.body.body4,
+            body5: request.body.body5,
+            body6: request.body.body6,
+            body7: request.body.body7,
+            body8: request.body.body8,
+            body9: request.body.body9,
+            body10: request.body.body10,
+            image_1_url: request.body.image_1_url,
+            image_2_url: request.body.image_2_url,
+            image_3_url: request.body.image_3_url,
+            image_4_url: request.body.image_4_url,
+            image_5_url: request.body.image_5_url,
+            image_6_url: request.body.image_6_url,
+            image_7_url: request.body.image_7_url,
+            image_8_url: request.body.image_8_url,
+            image_9_url: request.body.image_9_url,
+            image_10_url: request.body.image_10_url,
+            image_11_url: request.body.image_11_url,
+            image_12_url: request.body.image_12_url,
+            image_13_url: request.body.image_13_url,
+            image_14_url: request.body.image_14_url,
+            image_15_url: request.body.image_15_url,
+            image_16_url: request.body.image_16_url,
+            image_17_url: request.body.image_17_url,
+            image_18_url: request.body.image_18_url,
+            image_19_url: request.body.image_19_url,
+            image_20_url: request.body.image_20_url,
+        })
+        .returning('*')
+        .then(articles => response.json(articles[0]))
+})
+
+app.patch('/journal/:slug', (request, response) => {
+    const info = request.body
+    database('journal20')
+        .where({slug: request.params.slug})
+        .update(info)
+        .returning('*')
+        .then(articles => response.json(articles[0]))
+})
+
+app.delete('/journal/:slug', (request, response) => {
+    database('journal20')
+        .where({slug: request.params.slug})
+        .delete()
+        .then(() => response.status(204))
+})
+
 app.get('/fun', (request, response) => {
     response.send({message: 'whatever'})
 })
