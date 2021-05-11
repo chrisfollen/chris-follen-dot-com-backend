fc1f8f05ee8b1f739aec15bbb073de72bcf13294
Chris Follen <christopher.follen@gmail.com>
2021-05-11 16:19:45 -0600

    new config

--- knexfile.js
+++ knexfile.js
@@ -10,10 +10,8 @@ module.exports = {
     production: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
-      dialectOptions: {
-        ssl: { require: true,
-        rejectUnauthorized: false }
-      }
+      ssl: { rejectUnauthorized: false }
+    
     }
 
 }
